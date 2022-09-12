c5ba48741964fb39d458c7cb58b15d4a0568a238
AkhatorEnosa <aosakhogba@gmail.com>
2022-09-12 14:56:33 +0100

    responsiveness fix

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="w-full bg-[#BDE4F4] p-6 pt-8 logo">
       <div className="max-w-[1100px] mx-auto">
-        <h1 className="text-[#404969] lg:text-5xl text-3xl text-center">"_QuotesApp"</h1>
+        <h1 className="text-[#404969] lg:text-5xl text-4xl text-center tracking-tighter font-extrabold">Kwota</h1>
       </div>
     </div>
   )
--- src/containers/Quote.jsx
+++ src/containers/Quote.jsx
@@ -3,14 +3,17 @@ import Buttons from './Buttons';
 
 const Quote = (props) => {
   return (
-    <div className='w-full h-screen bg-[#BDE4F4] flex flex-col items-center px-6'>
+    <div className='w-full bg-[#BDE4F4] flex flex-col items-center px-6 pb-20'>
       <div className={props.status ? "animate-my-bounce" : "animate-my-bounce"}>
-        <div className="w-[300px] md:w-[700px] bg-[#404969] py-10 sm:px-10 px-8 rounded-2xl mt-40 shadow-md">
-          <span className='text-9xl absolute text-[#FF7F50] quote-mark left-[4rem] top-[14rem] tracking-widest'>&#10077;</span>
-          <h1 className='text-2xl md:text-3xl mb-5 text-[#BDE4F4] quote text-center'>{props.text}</h1>
-          <p className='text-[#FF7F50] author tracking-widest uppercase text-center'>-  {props.author}</p>
-          <span className='text-9xl absolute text-[#FF7F50] quote-mark right-[4rem] top-[14rem] tracking-widest -scale-x-100'>&#10077;</span>
-        </div>
+          <div className='w-[300px] md:w-[700px] flex justify-between top-[10rem] absolute mx-auto'>
+            <span className='text-9xl text-[#FF7F50] quote-mark tracking-widest'>&#10077;</span>
+            <span className='text-9xl text-[#FF7F50] quote-mark tracking-widest -scale-x-100'>&#10077;</span>
+          </div>
+
+          <div className="w-[300px] md:w-[700px] bg-[#404969] py-10 sm:px-10 px-8 rounded-2xl shadow-md mt-28 items-center">
+            <h1 className='text-2xl md:text-3xl mb-5 text-[#BDE4F4] quote text-center'>{props.text}</h1>
+            <p className='text-[#FF7F50] author tracking-widest uppercase text-center'>-  {props.author}</p>
+          </div>
       </div>
 
         <Buttons changeButton={props.changeQuote} copyButton={props.copyText}/>
