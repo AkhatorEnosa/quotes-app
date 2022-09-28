33f94289b86412012fb3812bd1c65d490dd456eb
Osa <aosakhogba@gmail.com>
2022-09-28 21:41:22 +0100

    Final fix

--- src/containers/Quote.jsx
+++ src/containers/Quote.jsx
@@ -17,7 +17,12 @@ const Quote = (props) => {
 
               <div className="w-[300px] md:w-[700px] bg-[#404969] py-10 sm:px-10 px-8 rounded-2xl shadow-md mt-28 items-center">
                 <h1 className='text-2xl md:text-3xl mb-5 text-[#BDE4F4] quote text-center'>{props.text}</h1>
-                </div>
+                <p className='text-[#FF7F50] author tracking-widest uppercase text-center'>-  {props.author}</p>
+              </div>
+
+              <div className='flex justify-end my-4 mx-0 text-[0.6rem] uppercase px-2 shadow-sm'>
+                <p className='bg-slate-400 w-fit text-[#BDE4F4] p-1 rounded-sm'>{props.tagName}</p>
+              </div>
           </div>
 
             <Buttons changeButton={props.changeQuote} capture={props.captureButton}/>
