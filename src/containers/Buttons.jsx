874021aa504bbb434fdf95530fc5e4ccd898a765
Osa <aosakhogba@gmail.com>
2022-10-21 17:14:56 +0100

    Button fix

--- src/containers/Buttons.jsx
+++ src/containers/Buttons.jsx
@@ -9,7 +9,7 @@ const Buttons = (props) => {
       <button
         className="bg-[#24cb2f] text-xs md:text-base px-4 py-3 rounded-md text-white shadow-md hover:text-white hover:bg-[#18ae47] duration-300 mx-5"
         onClick={props.capture}>
-        Download Screenshot
+        Download
       </button>
     </div>
   );
